37a955971edb1abac24ec0541b504fbbbffe57b7
Mohamed Aly <alydemah@gmail.com>
2021-07-22 00:09:12 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyDownloaderCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] lines = System.IO.File.ReadAllLines("links.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+
+                Process yt = new Process();
+                yt.StartInfo.FileName = "youtube-dl.exe";
+                yt.StartInfo.Arguments = line;
+
+                yt.StartInfo.UseShellExecute = false;
+                yt.StartInfo.RedirectStandardOutput = true;
+                yt.Start();
+
+                string output = yt.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+
+                yt.WaitForExit();
+
+
+            }
+        
+        }
+    }
+}
